137b82bcae0e67860b6245f756fce999a7e1fd9d
Wisnu Alfian Nur Ashar <wisnualfian117@gmail.com>
2026-09-08 09:05:46 +0700

    fix: update mail from address to mosque email

--- config/mail.php
+++ config/mail.php
@@ -111,8 +111,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', env('APP_NAME', 'Laravel')),
+        'address' => env('MAIL_FROM_ADDRESS', 'info@grandcenterpoint.id'),
+        'name' => env('MAIL_FROM_NAME', env('APP_NAME', 'Masjid Grand Centerpoint')),
     ],
 
 ];
